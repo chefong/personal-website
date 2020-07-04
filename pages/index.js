637581f5213cad5427f9dbbcdaef64ed05f60aca
ericong18 <ericong18@gmail.com>
2020-07-03 21:18:06 -0700

    Add og meta tag for description

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,18 @@ import Head from 'next/head';
 import Navigation from '../components/Navigation/Navigation';
 import Home from '../components/Home/Home';
 import Footer from '../components/Footer/Footer';
+import { aboutText } from '../content';
 
 export default function Root() {
   return (
     <>
       <Head>
         <title>Eric Ong</title>
+
         <link rel="icon" href="/favicon.ico" />
+        
         <meta property="og:title" content="Eric Ong" />
+        <meta property="og:description" content={`${aboutText.greetings} ${aboutText.greetingsSub1} ${aboutText.greetingsSub2}`} />
         <meta property="og:url" content="https://www.ericong.me/" />
         <meta property="og:image" content="/imgs/og-image.png" />
       </Head>
