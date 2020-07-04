2ff15b52d93b0995e4b087646ed80b2d83115238
ericong18 <ericong18@gmail.com>
2020-07-03 21:30:28 -0700

    Use relative path in og:image

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,19 @@ import Home from '../components/Home/Home';
 import Footer from '../components/Footer/Footer';
 import { aboutText } from '../content';
 
+const ogImage = require("../public/imgs/og-image.png");
+
 export default function Root() {
   return (
     <>
       <Head>
         <title>Eric Ong</title>
-
         <link rel="icon" href="/favicon.ico" />
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
         <meta property="og:title" content="Eric Ong" />
         <meta property="og:description" content={`${aboutText.greetings} ${aboutText.greetingsSub1} ${aboutText.greetingsSub2}`} />
         <meta property="og:url" content="https://www.ericong.me/" />
-        <meta property="og:image" content="/imgs/og-image.png" />
+        <meta property="og:image" content="../public/imgs/og-image.png" />
       </Head>
       <main>
         <Navigation />
