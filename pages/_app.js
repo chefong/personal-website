952fbe49f3471a06e8b2a83516e2b8c8c76577aa
ericong18 <ericong18@gmail.com>
2020-07-05 12:42:12 -0700

    Reorder css import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import './global-styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
+import './global-styles.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
