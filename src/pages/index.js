a53e138aba13e88174993e7a2ff5e73fadda5132
ericong18 <ericong18@gmail.com>
2020-07-09 18:02:10 -0700

    Use config file for next-seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import { NextSeo } from 'next-seo';
+import SEO from './next-seo.config';
 import Head from 'next/head';
 import Navigation from '../components/Navigation/Navigation';
 import Title from '../components/Title/Title';
@@ -5,7 +7,6 @@ import About from '../components/About/About';
 import Experience from '../components/Experience/Experience';
 import Projects from '../components/Projects/Projects';
 import Footer from '../components/Footer/Footer';
-import { NextSeo } from 'next-seo';
 
 export default function Root() {
   return (
@@ -14,18 +15,7 @@ export default function Root() {
         <link rel="icon" href="/favicon.ico" />
         <title>Eric Ong</title>
       </Head>
-      <NextSeo
-        title="Eric Ong"
-        description="I’m a Software Engineer based in the SF Bay Area with a focus on full-stack web development."
-        openGraph={{
-          type: "website",
-          url: "https://www.ericong.me/",
-          title: "Eric Ong",
-          site_name: "Eric Ong",
-          description: "I’m an aspiring Software Engineer based in the SF Bay Area with a focus on full-stack web development.",
-          images: [{ url: "https://www.ericong.me/imgs/og-image.png" }]
-        }}
-      />
+      <NextSeo {...SEO} />
       <main>
         <Navigation />
         <Title />
--- src/pages/next-seo.config.js
+++ src/pages/next-seo.config.js
@@ -4,7 +4,9 @@ export default {
   openGraph: {
     type: "website",
     url: "https://www.ericong.me/",
+    title: "Eric Ong",
     site_name: "Eric Ong",
-    description: "I’m an aspiring Software Engineer based in the SF Bay Area with a focus on full-stack web development."
+    description: "I’m an aspiring Software Engineer based in the SF Bay Area with a focus on full-stack web development.",
+    images: [{ url: "https://www.ericong.me/imgs/og-image.png" }]
   },
 };
