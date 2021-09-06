de16afcec20c039447b44fba486fdd0711d634d1
Eric Ong <ericong18@gmail.com>
2021-09-05 20:43:35 -0700

    Add hover for table row

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -1,38 +1,62 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import styles from './styles.module.scss';
 import Tag from '~/components/common/Tag';
 
 const locationTypeColors = {
   attraction: 'blue',
-  restaurant: 'red',
+  food: 'red',
 };
 
-const Location = ({ location, locations }) => (
-  <div className={styles.content}>
-    <div className={styles.container}>
-      <header>
-        <h1 className={styles.title}>
-          {`Recommendations for ${location}`}
-        </h1>
-      </header>
-      <table className={styles.table}>
-        <tr className={styles.tableRow}>
-          <th className={styles.tableHeader}>Name</th>
-          <th className={styles.tableHeader}>Type</th>
-        </tr>
-        {locations.map(({ name, type }) => (
-          <tr className={styles.tableRow}>
-            <td className={styles.tableElement}>{name}</td>
-            <td className={styles.tableElement}>
-              <Tag text={type} color={locationTypeColors[type]} />
-            </td>
-          </tr>
-        ))}
-      </table>
+const Location = ({ location, locations }) => {
+  const [rowHovered, setRowHovered] = useState({});
+
+  const toggleRowHover = (index, value) => {
+    setRowHovered((prevRowHovered) => ({ ...prevRowHovered, [index]: value }));
+  };
+
+  return (
+    <div className={styles.content}>
+      <div className={styles.container}>
+        <header>
+          <h1 className={styles.title}>
+            {`Recommendations for ${location}`}
+          </h1>
+        </header>
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.tableRow}>
+              <th className={styles.tableHeader}>Name</th>
+              <th className={styles.tableHeader}>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {locations.map(({ name, type }, index) => (
+              <tr
+                key={name}
+                className={
+                  classNames(
+                    styles.tableRow,
+                    rowHovered[index] && styles.tableRowHovered,
+                  )
+                }
+                onMouseEnter={() => toggleRowHover(index, true)}
+                onMouseLeave={() => toggleRowHover(index, false)}
+              >
+                <td className={styles.tableElement}>{name}</td>
+                <td className={styles.tableElement}>
+                  <Tag text={type} color={locationTypeColors[type]} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.mapContainer}>Map will go here</div>
     </div>
-    <div className={styles.mapContainer}>Map will go here</div>
-  </div>
-);
+  );
+};
 
 Location.propTypes = {
   location: PropTypes.string.isRequired,
